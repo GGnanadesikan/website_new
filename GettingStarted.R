a4ccc0e63ad51824266da5d0571ae0c04e69c429
Gitanjali Gnanadesikan <gitanjali.gnanadesikan@gmail.comm>
2021-05-20 10:17:15 -0700

    Initiate website

---
+++ GettingStarted.R
@@ -0,0 +1,12 @@
+#Getting started
+library(blogdown)
+
+new_site(theme = "gcushen/hugo-academic", 
+         sample = TRUE, 
+         theme_example = TRUE, 
+         empty_dirs = TRUE,
+         to_yaml = TRUE)
+
+hugo_version()
+
+file.create("netlify.toml")
